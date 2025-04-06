3f85980251a14501f36c1d4458916abe07db2dd0
plainionist <mail@plainionist.net>
2025-04-06 11:06:51 +0200

    code simplified

--- src/use_cases/chat.rs
+++ src/use_cases/chat.rs
@@ -10,32 +10,39 @@ pub fn run(config: &Config) {
     loop {
         print!("\nYou: ");
         io::stdout().flush().unwrap();
+
         let mut question = String::new();
         if io::stdin().read_line(&mut question).is_err() {
             break;
         }
+
         let question = question.trim();
         if question.is_empty() {
             continue;
         }
 
-        let question_embedding = ollama::embed_text(question).expect("Failed to embed question");
-
-        let top_chunks = similarity::find_similar_chunks(&chunks, &question_embedding, 8);
-        let context = top_chunks
-            .iter()
-            .map(|c| format!("• {}", c.text))
-            .collect::<Vec<_>>()
-            .join("\n\n");
-
-        let prompt = format!(
-            "Answer the following question based on my notes below.\n\
-            If not enough info is present, say so.\n\n\
-            Notes:\n{}\n\nQuestion: {}\n\nAnswer:",
-            context, question
-        );
-
-        let response = ollama::ask(&prompt);
+        let response = ask(&chunks, question);
         println!("\n🧠 >\n{}", response);
     }
+}
+
+fn ask(chunks: &Vec<chunking::Chunk>, question: &str) -> String {
+    let question_embedding = ollama::embed_text(question).expect("Failed to embed question");
+
+    let top_chunks = similarity::find_similar_chunks(chunks, &question_embedding, 10);
+
+    let context = top_chunks
+        .iter()
+        .map(|c| format!("- {}", c.text))
+        .collect::<Vec<_>>()
+        .join("\n\n");
+
+    let prompt = format!(
+        "Answer the following question based on my notes below.\n\
+            If not enough info is present, say so.\n\n\
+            Notes:\n{}\n\nQuestion: {}\n\nAnswer:",
+        context, question
+    );
+
+    ollama::ask(&prompt)
 }
